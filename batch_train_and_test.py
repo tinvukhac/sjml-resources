14357ab6e80fecdf5248b92e723cc1769e702686
Tin Vu <tinvukhac@gmail.com>
2021-06-19 19:54:03 -0700

    Update document

--- batch_train_and_test.py
+++ batch_train_and_test.py
@@ -20,11 +20,9 @@ def main():
             output_name = '{}distribution.{}'.format(r, name)
             train_path = 'data/train_and_test_all_features_split/train_join_results_combined_data.{}.csv'.format(output_name)
             print(train_path)
-            os.system('python main.py --model random_forest --tab {} --hist data/histograms/ --result data/join_results/train/join_results_small_x_small_uniform.csv --path trained_models/model_uniform.h5 --weights trained_models/model_weights_uniform.h5 --train'.format(train_path))
-            # os.system('python main.py --model random_forest --tab data/train_and_test_all_features_split/test_join_results_combined_data.csv --hist data/histograms/ --result data/join_results/train/join_results_small_x_small_uniform.csv --path trained_models/model_uniform.h5 --weights trained_models/model_weights_uniform.h5 --no-train')
+            os.system('python main.py --model random_forest --tab {} --path trained_models/model.h5 --weights trained_models/model_weights.h5 --target join_selectivity --train'.format(train_path))
             model = RegressionModel('random_forest')
-            mae, mape, mse, msle = model.test('data/train_and_test_all_features_split/test_join_results_combined_data.csv',
-                                                        '', 'trained_models/model_uniform.h5', 'trained_models/model_weights_uniform.h5', '')
+            mae, mape, mse, msle = model.test(test_path, 'join_selectivity', 'trained_models/model.h5', 'trained_models/model_weights.h5')
             train_results_f.writelines('{},{},{},{},{},{}\n'.format(r, name, mae, mape, mse, msle))
 
     train_results_f.close()
